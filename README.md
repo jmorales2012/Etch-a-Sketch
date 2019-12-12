233f6bb8700d64053d4d55d7bdce34b98fdfa4d0
Joshua <jmorales8191@gmail.com>
2019-12-12 17:22:45 +0100

    Added menu allowing reset, fading, and color selection

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Etch-A-Sketch project from [The Odin Project](https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project)
 
 [View Live](https://jmorales2012.github.io/etch-a-sketch/)
+
+
+Future Ideas for Project
+* Allow user to choose primary color to fill squares with
+* Allow user to save their board as image
+* Create database and let users upload/view/share/like their favorites
--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const container = document.querySelector('.container');
 container.style.height = '400px';
 container.style.width = '400px';
-createBoard();
-
 container.addEventListener('mouseover', draw);
+// let drawColor = '#444';
 
 const resetButton = document.querySelector('.reset');
 resetButton.addEventListener('click', resetBoard);
+
+const fadeButton = document.querySelector('.fade');
+fadeButton.addEventListener('click', changeEffect);
+
+const colorInput = document.querySelector('.draw-color');
 
 function createBoard(size = 5) {
   for (let i = 0; i < size * size; i++) {
@@ -19,17 +23,50 @@ function createBoard(size = 5) {
   }
 }
 
+function changeEffect(e) {
+  container.classList.toggle('fade');
+  fadeButton.classList.toggle('button-on');
+  fadeButton.classList.toggle('button-off');
+
+  if (container.classList.contains('fade')) {
+    fadeButton.textContent = 'Fade Effect On';
+  } else {
+    fadeButton.textContnt = 'Fade Effect Off';
+  }
+}
+
+function checkColor(color) {
+  if (!color) return false;
+  let test = new Option().style;
+  test.color = color;
+  console.log('test color: ' + test.color);
+  return test.color === color.toLowerCase();
+}
+
 function draw(e) {
   if (e.target.classList.contains('square')) {
-    e.target.style.backgroundColor = 'gray';
-    if (e.target.style.opacity < 1) {
-      e.target.style.opacity = Number(e.target.style.opacity) + 0.1;
-    } 
-  } 
-}
+    // see if user input is valid color
+    if (checkColor(colorInput.value)) {
+      e.target.style.backgroundColor = colorInput.value;
+    } else {
+      e.target.style.backgroundColor = '#444';
+    }
+
+    if (container.classList.contains('fade')) {
+      if (e.target.style.opacity < 1) {
+        e.target.style.opacity = Number(e.target.style.opacity) + 0.1;
+      } 
+    } else {
+      e.target.style.opacity = 1.0;
+    }
+  }
+} 
 
 function resetBoard() {
   let size = prompt('New Board Size: ');
   container.innerHTML = '';
+  colorInput.value = '';
   createBoard(size);
 }
+
+createBoard();
